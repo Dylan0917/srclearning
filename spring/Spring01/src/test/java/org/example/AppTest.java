89e384937e4a2eef003c8d53429bca0cd31c1b28
Dylan0917_Mac <46637586+Dylan0917@users.noreply.github.com>
2020-09-06 23:27:54 +0800

    提交

--- spring/Spring01/src/test/java/org/example/AppTest.java
+++ spring/Spring01/src/test/java/org/example/AppTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * Unit test for simple App.
@@ -14,6 +15,8 @@ public class AppTest
 //        获取容器对象
         ApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext applicationContext =
+//                new FileSystemXmlApplicationContext("/Users/ywh/CODESRC/srclearning/spring/Spring01/src/main/resources/applicationContext.xml");
 //        从容器对象中获取bean对象
         Person person = applicationContext.getBean(Person.class);
         System.out.println(person);
